a55529028c7e1881aaa434dc49bc6315a1df4fb6
wesleygdike <wesley.g.dike@gmail.com>
2019-05-25 11:13:41 -0400

    TouchMe 2.o

--- scripts/TouchMe.js
+++ scripts/TouchMe.js
@@ -1,5 +1,5 @@
 window.onload = setup;
-setInterval(Update, 1000/120);
+setInterval(Update, 1000 / 120);
 var drawAtX;
 var drawAtY;
 var drawRate = 120;
@@ -8,88 +8,107 @@ var drawCountMax = 60;
 var drawingEnabled = false;
 var particals = [];
 var particalCount = 0;
+var selectedShape = "square";
+var color;
+var fade;
 
-function setup(){
-    window.addEventListener("mousedown",myMouseStart);
-    window.addEventListener("mouseup", myMouseStop)
-    window.addEventListener("mousemove",myMouseMove);
-    window.addEventListener("touchmove",myTouchMove);
-    window.addEventListener("touchend",myMouseStop);
+function setup() {
+    /* mouse and touch inputs */
+    window.addEventListener("mousedown", myMouseStart);
+    window.addEventListener("mouseup", myMouseStop);
+    window.addEventListener("mousemove", myMouseMove);
+    window.addEventListener("touchmove", myTouchMove);
+    window.addEventListener("touchend", myMouseStop);
     document.body.style.userSelect = "none";
+    /* settings controls */
+    let shape = document.getElementById("shapeSetting");
+    let rate = document.getElementById("rateSetting");
+    let colorSelector = document.getElementById("colorSetting");
+    let faderate = document.getElementById("fadeSetting");
+    selectedShape = shape.value;
+    drawRate = rate.value;
+    color = colorSelector.value;
+    fade = faderate.value;
+    shape.addEventListener("change", function(event) {selectedShape = event.target.value});
+    rate.addEventListener("change", function(event) {drawRate = event.target.value});
+    colorSelector.addEventListener("change", function(event) {color = event.target.value});
+    faderate.addEventListener("change", function(event) {fade = event.target.value});
 }
 
-function drawing(){
-    if(!drawingEnabled) return;
-        document.getElementById("inputDisplayDrawing").innerHTML = drawAtX + ", " + drawAtY;
-        drawCount -= drawRate;
-        if(drawCount <= 0){
-            drawPartical(drawAtX,drawAtY);
-            drawCount = drawCountMax;
-        }
+function drawing() {
+    if (!drawingEnabled) return;
+    document.getElementById("inputDisplayDrawing").innerHTML = drawAtX + ", " + drawAtY;
+    drawCount -= drawRate;
+    if (drawCount <= 0) {
+        drawPartical(drawAtX, drawAtY);
+        drawCount = drawCountMax;
+    }
 }
 
-function drawingAt(x,y) {
-    drawAtX = x -25;
+function drawingAt(x, y) {
+    drawAtX = x - 25;
     drawAtY = y;
 }
 
-function drawPartical(x,y){
-    let partical = new Partical(x,y);
+function drawPartical(x, y) {
+    let partical = new Partical(x, y);
     partical.animate();
     particals.push(partical);
 }
 
 function myTouchMove(event) {
     var x = event.touches[0].clientX;
-    var y = event.touches[0].clientY -25;
+    var y = event.touches[0].clientY - 25;
     document.getElementById("inputDisplay").innerHTML = x + ", " + y;
     drawingEnabled = true;
-    drawingAt(x,y);
-  }
+    drawingAt(x, y);
+}
 
-  function myMouseStart(event) {
-      var x = event.screenX;
-      var y = event.screenY  -85;
-      document.getElementById("inputDisplayMouse").innerHTML = x + ", " + y;
-      drawingEnabled = true;
-      drawingAt(x,y);
-  }
-
-  function myMouseMove(event) {
+function myMouseStart(event) {
     var x = event.screenX;
-    var y = event.screenY  -85;
-      drawingAt(x,y);
-  }
+    var y = event.screenY - 85;
+    document.getElementById("inputDisplayMouse").innerHTML = x + ", " + y;
+    drawingEnabled = true;
+    drawingAt(x, y);
+}
 
-  function myMouseStop(event) {
-      drawingEnabled = false;
-  }
+function myMouseMove(event) {
+    var x = event.screenX;
+    var y = event.screenY - 85;
+    drawingAt(x, y);
+}
 
-function Update(){
+function myMouseStop(event) {
+    drawingEnabled = false;
+}
+
+function Update() {
     drawing();
 }
 
-function dropOldPartical(item, index){
-    
+function dropOldPartical(item, index) {
+
 }
 
-function Partical(x,y){
+function Partical(x, y) {
     this.id = "Partical" + particalCount++;
     this.body = document.createElement("div");
     this.body.id = this.id;
     this.body.style.position = "fixed";
     this.body.style.left = x + "px";
     this.body.style.top = y + "px";
-    this.body.classList.add("square");
+    this.body.classList.add(selectedShape);
+    this.body.style.border = "solid 1px " + color;
     document.body.appendChild(this.body);
 
-    this.animate = function(){
-        this.body.classList.add("show");
+    this.animate = function () {
+        this.body.style.animationName = "freefadein, freefadeout";
+        this.body.style.animationDuration = fade+"s";
         this.body.addEventListener("webkitAnimationEnd", this.killit);
     }
-    this.killit = function(){
+    this.killit = function () {
         let blackSpot = particals.findIndex(function (element) { return element.id == this.id; })
         particals.splice(blackSpot, 1);
         document.getElementById(this.id).remove();
-    }    
+    }
 }
